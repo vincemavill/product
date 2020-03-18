248d87b1f054e024515199179685c8191b4d7a06
vince <vincemavill@gmail.com>
2020-03-17 18:37:35 -0700

    v 1.0.0

---
+++ resources/views/home.blade.php
@@ -0,0 +1,333 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{url('/')}}/public/stylesheet.css" >
+
+    <title>“Protect your staff, guest and public”</title>
+  </head>
+  <body>
+    <div class="bg_c1">
+        <div class="container py-5">
+        <h1 class=" text-white text-center">
+        “Protect your staff, guest and public”
+        </h1>
+        </div>
+    </div>
+    <div class="container py-5">
+        <h1 class=" text-dark text-center display-4 my-5">
+        50 Day Early Containment
+        & Prevention Kit
+        </h1>
+
+
+
+
+
+
+    <div>
+        
+    <div class="row py-5">
+            <div class="col-lg-6 align-items-center d-flex">
+                <div class="d-block mx-auto">
+                    <h3 class="text_c1">Package 1</h3>
+                    <div class="text-center">
+                        <p class="text-nowrap d-inline-block mb-0">
+                            <h1 class="d-inline-block text_c1 mb-0">$</h1>
+                            <h1 class="d-inline-block display-1 text_c1 mb-0"><b>195</b></h1>
+                            <h1 class="d-inline-block text_c1 mb-0">inc</h1>
+                        </p>
+                    </div>
+                    <div class="bg_c1 py-1 my-2">
+                    </div>
+                  
+                    <div class="text-center">
+                        <h3 class="text_c1">LIMITED STOCK 500 only</h3>
+                    </div>
+
+                    <div class="row py-3">
+                        <div class="col-lg-3">
+                            <img src="{{url('/')}}/public/img/icon(1).png" class="mx-auto d-block" width="60">
+                        </div>
+                        <div class="col-lg-3">
+                            <img src="{{url('/')}}/public/img/icon(2).png" class="mx-auto d-block" width="60">
+                        </div>
+                        <div class="col-lg-3">
+                            <img src="{{url('/')}}/public/img/icon(3).png" class="mx-auto d-block" width="60">
+                        </div>
+                        <div class="col-lg-3">
+                            <img src="{{url('/')}}/public/img/icon(4).png" class="mx-auto d-block" width="60">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-6">
+                <img src="{{url('/')}}/public/img/img(1).jpg" class="mx-auto d-block p-4 img-fluid">
+            </div>
+
+        
+        </div>
+
+        <div class="py-4 list_cont">
+            <h3>
+            Kit Includes:
+            </h3>
+            <div class="py-3">
+
+            <h4>
+            <img src="{{url('/')}}/public/img/check.svg" class="mr-3" width="40">
+            Guide how to 1 2 3…. safely sanitising workplace surfaces & touch points
+            </h4>
+
+            <h4>
+            <img src="{{url('/')}}/public/img/check.svg" class="mr-3" width="40">
+            Guide how to 1 2 3…. safely sanitising workplace surfaces & touch points
+            </h4>
+
+            </div>
+        </div>
+
+        <div class="row py-5 justify-content-center">
+            <div class="col-lg-4">
+            <a href="#" class="btn btn_c1 btn-lg  w_700 mx-auto" role="button" aria-pressed="true">
+                Add to cart
+            </a>
+            </div>
+            <div class="col-lg-4">
+            <a href="#" class="btn btn_c2 btn-lg  w_700 mx-auto" role="button" aria-pressed="true">
+                Buy Now
+            </a>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div>
+        
+        <div class="row py-5">
+                <div class="col-lg-6 align-items-center d-flex">
+                    <div class="d-block mx-auto">
+                        <h3 class="text_c1">Package 1</h3>
+                        <div class="text-center">
+                            <p class="text-nowrap d-inline-block mb-0">
+                                <h1 class="d-inline-block text_c1 mb-0">$</h1>
+                                <h1 class="d-inline-block display-1 text_c1 mb-0"><b>195</b></h1>
+                                <h1 class="d-inline-block text_c1 mb-0">inc</h1>
+                            </p>
+                        </div>
+                        <div class="bg_c1 py-1 my-2">
+                        </div>
+                      
+                        <div class="text-center">
+                            <h3 class="text_c1">LIMITED STOCK 500 only</h3>
+                        </div>
+    
+                        <div class="row py-3">
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(1).png" class="mx-auto d-block" width="60">
+                            </div>
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(2).png" class="mx-auto d-block" width="60">
+                            </div>
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(3).png" class="mx-auto d-block" width="60">
+                            </div>
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(4).png" class="mx-auto d-block" width="60">
+                            </div>
+                        </div>
+                    </div>
+    
+                </div>
+                <div class="col-lg-6">
+                    <img src="{{url('/')}}/public/img/img(1).jpg" class="mx-auto d-block p-4 img-fluid">
+                </div>
+    
+            
+            </div>
+    
+            <div class="py-4 list_cont">
+                <h3>
+                Kit Includes:
+                </h3>
+                <div class="py-3">
+    
+                <h4>
+                <img src="{{url('/')}}/public/img/check.svg" class="mr-3" width="40">
+                Guide how to 1 2 3…. safely sanitising workplace surfaces & touch points
+                </h4>
+    
+                <h4>
+                <img src="{{url('/')}}/public/img/check.svg" class="mr-3" width="40">
+                Guide how to 1 2 3…. safely sanitising workplace surfaces & touch points
+                </h4>
+    
+                </div>
+            </div>
+    
+            <div class="row py-5 justify-content-center">
+                <div class="col-lg-4">
+                <a href="#" class="btn btn_c1 btn-lg  w_700 mx-auto" role="button" aria-pressed="true">
+                    Add to cart
+                </a>
+                </div>
+                <div class="col-lg-4">
+                <a href="#" class="btn btn_c2 btn-lg  w_700 mx-auto" role="button" aria-pressed="true">
+                    Buy Now
+                </a>
+                </div>
+            </div>
+    
+        </div>
+
+        
+
+        <div>
+        
+        <div class="row py-5">
+                <div class="col-lg-6 align-items-center d-flex">
+                    <div class="d-block mx-auto">
+                        <h3 class="text_c1">Package 1</h3>
+                        <div class="text-center">
+                            <p class="text-nowrap d-inline-block mb-0">
+                                <h1 class="d-inline-block text_c1 mb-0">$</h1>
+                                <h1 class="d-inline-block display-1 text_c1 mb-0"><b>195</b></h1>
+                                <h1 class="d-inline-block text_c1 mb-0">inc</h1>
+                            </p>
+                        </div>
+                        <div class="bg_c1 py-1 my-2">
+                        </div>
+                      
+                        <div class="text-center">
+                            <h3 class="text_c1">LIMITED STOCK 500 only</h3>
+                        </div>
+    
+                        <div class="row py-3">
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(1).png" class="mx-auto d-block" width="60">
+                            </div>
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(2).png" class="mx-auto d-block" width="60">
+                            </div>
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(3).png" class="mx-auto d-block" width="60">
+                            </div>
+                            <div class="col-lg-3">
+                                <img src="{{url('/')}}/public/img/icon(4).png" class="mx-auto d-block" width="60">
+                            </div>
+                        </div>
+                    </div>
+    
+                </div>
+                <div class="col-lg-6">
+                    <img src="{{url('/')}}/public/img/img(1).jpg" class="mx-auto d-block p-4 img-fluid">
+                </div>
+    
+            
+            </div>
+    
+            <div class="py-4 list_cont">
+                <h3>
+                Kit Includes:
+                </h3>
+                <div class="py-3">
+    
+                <h4>
+                <img src="{{url('/')}}/public/img/check.svg" class="mr-3" width="40">
+                Guide how to 1 2 3…. safely sanitising workplace surfaces & touch points
+                </h4>
+    
+                <h4>
+                <img src="{{url('/')}}/public/img/check.svg" class="mr-3" width="40">
+                Guide how to 1 2 3…. safely sanitising workplace surfaces & touch points
+                </h4>
+    
+                </div>
+            </div>
+    
+            <div class="row py-5 justify-content-center">
+                <div class="col-lg-4">
+                <a href="#" class="btn btn_c1 btn-lg  w_700 mx-auto" role="button" aria-pressed="true">
+                    Add to cart
+                </a>
+                </div>
+                <div class="col-lg-4">
+                <a href="#" class="btn btn_c2 btn-lg  w_700 mx-auto" role="button" aria-pressed="true">
+                    Buy Now
+                </a>
+                </div>
+            </div>
+    
+        </div>
+    
+
+
+        <div class="py-5">
+
+            <p class="text-dark">
+            Don't place your employees at risk, cleaning without a documented procedure, the correct PPE and method. Our kit provides all this so that early containment and prevention can be achieved.
+            </p>
+            
+            <p class="text-dark">
+            The 50 day kit provides 3 times daily sanitation to your venue. Isolating your cleaning kit away 
+from your current cleaning area preventing cross contamination and risk.
+
+            </p class="text-muted">
+
+            <div class="py-3"></div>
+
+            <p class="text-muted">
+            “The analysis of 22 studies reveals that human coronaviruses such as Severe Acute Respiratory Syndrome (SARS) coronavirus, Middle East Respiratory Syndrome (MERS) coronavirus or endemic human coronaviruses (HCoV) can persist on inanimate surfaces like metal, glass or plastic for up to 9 days, but can be efficiently inactivated by surface disinfection procedures with 62-71% ethanol, 0.5% hydrogen peroxide or 0.1% sodium hypochlorite within 1 minute. Other biocidal agents such as 0.05-0.2% benzalkonium chloride or 0.02% chlorhexidine digluconate are less effective. As no specific therapies are available
+for SARS-CoV-2, early containment and prevention of further spread will be crucial to stop the ongoing outbreak and
+to control this novel infectious thread”
+
+            </p>
+        </div>
+
+
+
+    </div>
+
+    <div class="footer bg_c1 pt-5">
+        <div class="footer_content">
+            <h4 class="text-white">ABSTRACT</h4>
+            <p class="text-white mb-0">
+                <small>
+                Currently, the emergence of a novel human coronavirus, SARS-CoV-2, has become a global health concern causing severe respiratory tract infections in humans. Human-to-human transmissions have been described with incubation times between 2-10 days, facilitating its spread via droplets, contaminated hands or surfaces. We therefore reviewed the literature on all available information about the persistence of human and veterinary coronaviruses on inanimate surfaces as well as inactivation strategies with biocidal agents used for chemical disinfection, e.g. in healthcare facilities. The analysis of 22 studies reveals that human coronaviruses such as Severe Acute Respiratory Syndrome (SARS) coronavirus, Middle East Respiratory Syndrome (MERS) coronavirus or endemic human coronaviruses (HCoV) can persist on inanimate surfaces like metal, glass or plastic for up to 9 days, but can be efficiently inactivated by surface disinfection procedures with 62-71% ethanol, 0.5% hydrogen peroxide or 0.1% sodium hypochlorite within 1 minute. Other biocidal agents such as 0.05-0.2% benzalkonium chloride or 0.02% chlorhexidine digluconate are less effective. As no specific therapies are available for SARS-CoV-2, early containment and prevention of further spread will be crucial to stop the ongoing outbreak and to control this novel infectious thread.
+                
+                </small>
+            </p>
+            <div class="footer_lin mt-4"></div>
+            <p class="mb-0 text-center text-white py-4">
+                <small>
+                Copyright © 2020 The Healthcare Infection Society. Published by Elsevier Ltd. All rights reserved.
+                </small>
+            </p>
+        </div>
+    </div>
+
+    
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
